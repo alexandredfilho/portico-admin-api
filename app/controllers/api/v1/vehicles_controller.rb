a3a03c6340a8ebfd25e5b4b1dbbdb172bec67418
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-04-25 22:23:46 -0300

    Request user authentication in vehicles controller

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class VehiclesController < ApplicationController
+      before_action :authenticate_user!
       before_action :set_vehicle, only: %i[show update destroy]
 
       def index
