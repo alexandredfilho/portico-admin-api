d2d30c091026f96e97163b22b8a264dc67e789e7
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-04-25 22:23:36 -0300

    Request user authentication in shipments controller

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class ShipmentsController < ApplicationController
+      before_action :authenticate_user!
       before_action :set_shipment, only: %i[show update destroy]
 
       def index
