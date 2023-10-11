f66861ce5f76579a585a81c5baafbe11abf0adad
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-10 22:18:38 -0300

    Add annotations

--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: vehicles
+#
+#  id            :bigint           not null, primary key
+#  bodywork      :enum             not null
+#  license_plate :string
+#  manufacturer  :string
+#  sample        :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 require "test_helper"
 
 class VehicleTest < ActiveSupport::TestCase
