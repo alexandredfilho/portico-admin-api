8c9ce8871d6a37a10bb4e084a7763356cc2f4161
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-12 10:17:55 -0300

    Change column name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,15 +2,15 @@
 #
 # Table name: customers
 #
-#  id          :bigint           not null, primary key
-#  description :string
-#  status      :enum             default("active"), not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id         :bigint           not null, primary key
+#  status     :enum             default("active"), not null
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 class Customer < ApplicationRecord
   has_many :shipments
-  validates_presence_of :description
+  validates_presence_of :title
 
   enum :status, { active: "active", inactive: "inactive" }, default: "active"
 end
