61c7c1eb67a6d4bb3cd57d695552617cd7a7669f
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-13 10:34:20 -0300

    Update annotations

--- spec/factories/drivers.rb
+++ spec/factories/drivers.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: drivers
+#
+#  id               :bigint           not null, primary key
+#  document         :string           not null
+#  first_name       :string           not null
+#  full_name        :string
+#  last_name        :string
+#  shipping_company :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 FactoryBot.define do
   factory :driver do
     document { "123456789" }
--- spec/factories/shipments.rb
+++ spec/factories/shipments.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: shipments
+#
+#  id             :bigint           not null, primary key
+#  cargo_checker  :string           not null
+#  dock           :string           not null
+#  invoice_number :string           not null
+#  kind           :enum             not null
+#  status         :enum             not null
+#  warehouse      :enum             not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  customer_id    :bigint           not null
+#  driver_id      :bigint           not null
+#  vehicle_id     :bigint           not null
+#
+# Indexes
+#
+#  index_shipments_on_customer_id  (customer_id)
+#  index_shipments_on_driver_id    (driver_id)
+#  index_shipments_on_vehicle_id   (vehicle_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (customer_id => customers.id)
+#  fk_rails_...  (driver_id => drivers.id)
+#  fk_rails_...  (vehicle_id => vehicles.id)
+#
 FactoryBot.define do
   factory :shipment do
     association :customer, factory: :customer
