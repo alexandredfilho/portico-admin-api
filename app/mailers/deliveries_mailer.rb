e388d60452b6727719032183a89e579eec2cc508
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-13 21:24:16 -0300

    Add the first mailer

---
+++ app/mailers/deliveries_mailer.rb
@@ -0,0 +1,14 @@
+class DeliveriesMailer < ApplicationMailer
+  #
+  # This method is responsible for send an email
+  #
+  # @param [shipment] shipment
+  #
+  # @return [void]
+  #
+  def delivery_completed(shipment)
+    @shipment = shipment
+
+    mail to: "to@example.org"
+  end
+end
