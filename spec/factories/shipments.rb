5ed02da3184abce2881b749bfc906e7b308e9805
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-14 14:09:11 -0300

    Fix cops with rubocop

--- spec/factories/shipments.rb
+++ spec/factories/shipments.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :shipment do
     association :customer, factory: :customer
     association :driver, factory: :driver
     association :vehicle, factory: :vehicle
 
-    cargo_checker { 'John Doe' }
-    dock { '2' }
     invoice_number { '12345' }
-    kind { 'delivery' }
-    warehouse { 'high-tech' }
+    kind { 'receive' }
+    warehouse { 'high_tech' }
+    dock { '2' }
+    cargo_checker { 'John Doe' }
+    status { 'pending' }
   end
 end
 
