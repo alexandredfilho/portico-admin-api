3ecff6fe1f98b03883fc5a0ef11394392e6943c4
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-18 11:22:12 -0300

    Add cancancan error rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
+#
+# This is the base controller
+#
 class ApplicationController < ActionController::API
+  include CanCan::ControllerAdditions
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { message: exception.message }, status: :forbidden
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  end
 end
