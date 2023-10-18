1c7295956ff29ec883ab56eea19f985d3371ffe6
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-18 11:26:52 -0300

    Refactoring  in the sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,35 +1,51 @@
 # frozen_string_literal: true
 
-class Users::SessionsController < Devise::SessionsController
-  respond_to :json
+module Users
+  #
+  # This controller is responsible  for creating users sessions
+  #
+  class SessionsController < Devise::SessionsController
+    respond_to :json
 
-  private
+    private
 
-  def respond_with(_resource, _options = {})
-    render json: {
-             status: {
-               code: 200,
-               message: 'User signed in successfully',
-               data: current_user
-             }
-           },
-           status: :ok
-  end
+    def respond_with(_resource, _options = {})
+      render json: {
+               status: {
+                 code: 200,
+                 message: 'User signed in successfully',
+                 data: current_user
+               }
+             },
+             status: :ok
+    end
 
-  def respond_to_on_destroy
-    jwt_payload =
+    def respond_to_on_destroy
+      current_user = User.find(jwt_payload['sub'])
+
+      if current_user
+        user_session_successfully_registered
+      else
+        user_session_failed_to_register
+      end
+    end
+
+    def jwt_payload
       JWT.decode(
         request.headers['Authorization'].split(' ')[1],
         Rails.application.credentials.fetch(:secret_key_base)
       ).first
-    current_user = User.find(jwt_payload['sub'])
-    if current_user
+    end
+
+    def user_session_successfully_registered
       render json: {
                status: 200,
                message: 'Signed out successfully'
              },
              status: :ok
-    else
+    end
+
+    def user_session_failed_to_register
       render json: {
                status: 401,
                message: 'User has no active session'
