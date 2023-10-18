f3c44fd4b5511753202d2b8c27ed723aa2d3ff91
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-18 11:17:43 -0300

    Add abilities to each role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,5 +6,26 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user); end
+  def initialize(user)
+    send("#{user.role}_abilities", user)
+  end
+
+  def admin_abilities(_user)
+    can :manage, :all
+  end
+
+  def gate_agent_abilities(_user)
+    can %i[create read update destroy], Driver
+    can %i[create read update destroy], Vehicle
+    can %i[read], Customer
+    can :manage, Shipment
+  end
+
+  def dock_agent_abilities(_user)
+    can %i[update], Shipment
+  end
+
+  def user_abilities(_user)
+    can :read, :all
+  end
 end
