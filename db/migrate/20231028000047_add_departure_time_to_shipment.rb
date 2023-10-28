039e93e138c24edd365e433fc0b635cc0ded547f
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-27 21:52:03 -0300

    Add new filed to the Shipment model

---
+++ db/migrate/20231028000047_add_departure_time_to_shipment.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddDepartureTimeToShipment < ActiveRecord::Migration[7.1]
+  def up
+    add_column :shipments, :departure_time, :string
+  end
+
+  def down
+    remove_column :shipments, :departure_time
+  end
+end
