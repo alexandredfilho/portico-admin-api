819a38b985ef4d60cc8655aa2dc4c52f548fac00
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-10-30 21:53:42 -0300

    Create a migration to add user to shipment

---
+++ db/migrate/20231030152326_add_user_to_shipment.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddUserToShipment < ActiveRecord::Migration[7.1]
+  def up
+    add_reference :shipments, :user, null: false, foreign_key: true
+  end
+
+  def down
+    remove_references :shipments, :user
+  end
+end
