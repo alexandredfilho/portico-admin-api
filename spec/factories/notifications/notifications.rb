212d77cd447b09a1a87a6b104040b1c731e150e6
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2023-12-29 17:03:51 -0300

    Fix notifications factory

--- spec/factories/notifications/notifications.rb
+++ spec/factories/notifications/notifications.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :notification, class: 'Notifications::Notification' do
-    user { create(:accounts_user) }
+    user { create(:user) }
     type { "Notifications::InApp" }
     message { "Hey! New message!" }
   end
