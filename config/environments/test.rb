ebbb9bda046d43b36657159f2760dbdb9f60838e
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-01-16 21:45:37 -0300

    Add test settings and sidekiq setting

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Used to test notifications queue
+  config.active_job.queue_adapter = :test
 end
