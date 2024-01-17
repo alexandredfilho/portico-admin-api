3042717eaf64fb2daffa65d5e0804381ed95089d
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-01-16 21:40:45 -0300

    Create notification in app

---
+++ app/models/notifications/in_app.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Notifications
+  #
+  # InApp Notification
+  #
+  class InApp < Notification
+  end
+end
+
+# == Schema Information
+#
+# Table name: notifications_notifications
+#
+#  id         :bigint           not null, primary key
+#  action     :string
+#  icon       :string
+#  message    :string
+#  priority   :string
+#  read       :boolean
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_notifications_notifications_on_user_id_and_read  (user_id,read)
+#
