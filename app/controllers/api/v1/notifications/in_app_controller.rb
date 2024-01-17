0eb6cb38ffdfd217e5edde368ac8a98c106d18fe
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-01-16 21:41:05 -0300

    Add in app notification controller

---
+++ app/controllers/api/v1/notifications/in_app_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+#
+# API controller for In-App Notifications
+#
+module Api
+  module V1
+    module Notifications
+      class InAppsController < ApplicationController
+      end
+    end
+  end
+end
